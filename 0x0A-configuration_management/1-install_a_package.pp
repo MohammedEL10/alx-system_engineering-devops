20855a7098f5439d6b1cf3ca17da1f5d5d6ca32d
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-23 00:47:00 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# /usr/bin/pup
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0",
-}
-
-exec { 'install_werkzeug':
-  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
-  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1",
+# install flask from pip3
+package { 'puppet_lint':
+  ensure => '2.1.0',
+  provider => 'pip3'
 }
