f6af1de154155739f6115c75e428b0a2b843cdb4
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-23 00:14:54 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
 # usr/bin/pup
+# install an especific version of flask (2.1.0)
+# install an especific version of werkzeug (2.1.1)
 package { 'puppet-lint':
   ensure   => '2.1.0',
   provider => 'pip3'
