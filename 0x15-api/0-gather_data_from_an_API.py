7c31e1cfa4c76584576407a72bcea54b99cf1c23
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-23 22:12:48 -0700

    0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
 import urllib
 Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
     EMPLOYEE_NAME
