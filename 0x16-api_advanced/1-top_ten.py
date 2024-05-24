8b54f673083101a9dd608bb4736d9d76a7dd656f
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-05-24 12:06:10 -0700

    api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit."""
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
                             .format(subreddit),
